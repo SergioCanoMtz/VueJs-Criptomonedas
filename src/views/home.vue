28387cf7361941b0e8935ee2630064554493129d
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-05-28 22:41:43 -0500

    First commit

---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+        <px-assets-table />
+    </div>
+</template>
+
+<script>
+    import PxAssetsTable from "@/components/PxAssetsTable";
+
+    export default {
+        name: 'Home',
+        components:{
+            PxAssetsTable
+        }
+    };
+</script>
