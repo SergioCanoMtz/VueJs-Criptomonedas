16c2c49c1839c8b188b5fcb31f46c7adfeb28cf3
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-05-29 17:45:54 -0500

    fetch Api

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home'
 import About from '@/views/about'
+import Errorr from '@/views/error'
 
 Vue.use(Router)
 
@@ -17,6 +18,12 @@ export default new Router({
             path: '/about',
             name: 'about',
             component: About
+        },
+        {
+            path: '*',
+            name: 'error',
+            component: Errorr
         }
+
     ]
 })
--- src/views/error.vue
+++ src/views/error.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="flex-col items-center">
-        <h1 class="text-gray-700 text-6xl mb-5">Cripto Cano v1.0.0</h1>
+        <h1 class="text-gray-700 text-6xl uppercase mb-5">
+            Error
+        </h1>
         <p class="text-gray-600 text-xl text-center">
-            Proyecto web app para obtener las cotizaciones de las 
-            criptomonedas  mas importantes a traves de la API REST 
-            de Coincap.</p>
-
+            La ruta a la cual intentas acceder no es valida.</p>
         <router-link to="/" class="mt-5 text-xl text-blue-700 hover:underline"
         >Volver al Inicio</router-link>
     </div>
