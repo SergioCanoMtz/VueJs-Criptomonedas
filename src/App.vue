d0206c711fdec71527277e44aec7d39a7f4dbe73
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-06-16 12:32:25 -0500

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <main>
     <px-header />
-
     <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
@@ -19,11 +18,11 @@
 
 <style>
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
   }
 </style>
