01552b8e17a4831de2bb92549609be41346a9c25
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-06-23 22:46:10 -0500

    CoinDetail

--- src/views/coindetail.vue
+++ src/views/coindetail.vue
@@ -42,18 +42,15 @@
         </div>
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
-          <button
-            class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
           >cambiar</button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
-
                 id="convertValue"
                 type="number"
-                class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
-                
+                class="text-center bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"         
               />
             </label>
           </div>
